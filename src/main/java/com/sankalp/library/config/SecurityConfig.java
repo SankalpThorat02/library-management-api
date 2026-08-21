eadf992c668e951f5590f0bce20be7e12a271c8e
SankalpThorat02 <sankalpt2225@gmail.com>
2026-08-21 14:49:44 +0530

    fix: add the custom authentication Entry point for authentication exception

--- src/main/java/com/sankalp/library/config/SecurityConfig.java
+++ src/main/java/com/sankalp/library/config/SecurityConfig.java
@@ -3,10 +3,12 @@ package com.sankalp.library.config;
 import com.sankalp.library.filter.JwtAuthenticationFilter;
 import com.sankalp.library.service.CustomUserDetailsService;
 import com.sankalp.library.service.JwtService;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -45,10 +47,18 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http, JwtAuthenticationFilter jwtAuthenticationFilter) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
-                .sessionManagement((session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)))
+
+                .sessionManagement((session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)))
+
+                .exceptionHandling(exception -> exception
+                        .authenticationEntryPoint((request, response, authException) -> {
+                            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException.getMessage());
+                        }))
+
                 .authorizeHttpRequests(auth -> auth
-                    .requestMatchers("/auth/login").permitAll()
-                    .anyRequest().authenticated())
+                        .requestMatchers("/auth/login").permitAll()
+                        .anyRequest().authenticated())
 
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
